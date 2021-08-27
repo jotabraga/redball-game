69aa613042248f3ddedfa2e88a0af164ceb5f821
jotabraga <jpmbraga.feg@gmail.com>
2021-08-26 23:50:40 -0300

    Add webpack config

---
+++ Enemy.js
@@ -0,0 +1,32 @@
+class Enemy {
+    x;
+    y;
+    radius = 15,
+    color = "blue",
+    speedX = Math.floor(Math.random()*10 + 5),
+    speedY = Math.floor(Math.random()*10 + 5),
+
+    constructor(initialX, initialY) {
+      this.x = initialX;
+      this.y = initialY;
+    }
+
+    bounceOnEdge() {
+      if (this.x < 0 || this.x > screenWidth) {
+        this.speedX *= -1.001;
+      }
+
+      if (this.y < 0 || this.y > screenHeight) {
+        this.speedY *= -1.001;
+      }
+    }
+
+    draw() {
+      drawCircle(this.x, this.y, this.radius, this.color);
+    }
+
+    move() {
+      this.x += this.speedX;
+      this.y += this.speedY;
+    }
+  }
