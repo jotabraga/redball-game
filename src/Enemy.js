b346693ea16bdf89afb144563e29d54f7e33a48c
jotabraga <jpmbraga.feg@gmail.com>
2021-09-24 16:46:22 -0300

    reafactor: enemy class refactor

--- src/Enemy.js
+++ src/Enemy.js
@@ -1,32 +1,20 @@
-class Enemy {
-    x;
-    y;
-    radius = 15;
-    color = "blue";
-    speedX = Math.floor(Math.random()*10 + 5);
-    speedY = Math.floor(Math.random()*10 + 5);
+class Enemy extends AutonomousBall {
+  constructor(context) {
+    super(context, 20, "blue");
+  }
 
-    constructor(initialX, initialY) {
-      this.x = initialX;
-      this.y = initialY;
+  bounceOnEdge() {
+    if (this.x < 0 || this.x > screenWidth) {
+      this.speedX *= -1;
     }
 
-    bounceOnEdge() {
-      if (this.x < 0 || this.x > screenWidth) {
-        this.speedX *= -1.001;
-      }
-
-      if (this.y < 0 || this.y > screenHeight) {
-        this.speedY *= -1.001;
-      }
-    }
-
-    draw() {
-      drawCircle(this.x, this.y, this.radius, this.color);
-    }
-
-    move() {
-      this.x += this.speedX;
-      this.y += this.speedY;
+    if (this.y < 0 || this.y > screenHeight) {
+      this.speedY *= -1;
     }
   }
+
+  increaseSpeed() {
+    this.speedX *= 1.0001;
+    this.speedY *= 1.0001;
+  }
+}
