7b49ca2d08c997bca59331f4507922c2905dc324
jotabraga <jpmbraga.feg@gmail.com>
2021-09-24 16:48:13 -0300

    feat: player class implementation

---
+++ src/Player.js
@@ -0,0 +1,16 @@
+class Player extends Ball {
+    constructor(context, initialX, initialY) {
+      super(context, initialX, initialY, 50, "red");
+    }
+  
+    moveTo(x, y) {
+      this.x = x;
+      this.y = y;
+    }
+  
+    checkCollision(ball) {
+      const distance = Math.sqrt((this.x - ball.x) ** 2 + (this.y - ball.y) ** 2);
+  
+      return distance < this.radius + ball.radius;
+    }
+  }
